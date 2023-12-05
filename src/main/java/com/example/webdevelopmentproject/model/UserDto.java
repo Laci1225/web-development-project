850e6973b71c91f6b59a8f59d906e7766ea18399
Nagy László Levente <nagylacika2002@gmail.com>
2023-12-05 19:55:33 +0100

    UserDto hotfix

--- src/main/java/com/example/webdevelopmentproject/model/UserDto.java
+++ src/main/java/com/example/webdevelopmentproject/model/UserDto.java
@@ -1,6 +1,8 @@
 package com.example.webdevelopmentproject.model;
 
 import com.example.webdevelopmentproject.persistence.entity.Order;
+import com.example.webdevelopmentproject.persistence.entity.Role;
+import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,4 +18,6 @@ public class UserDto {
     private String email;
     private String password;
     private List<Order> orders;
+    @Enumerated
+    private Role role;
 }
